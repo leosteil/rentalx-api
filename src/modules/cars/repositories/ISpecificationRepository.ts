13299261f5af0e8c03da329a13af6148039463bd
leosteil <lsteil@inf.ufsm.br>
2021-05-04 13:22:42 -0300

    Creating use cases and applying solid concepts

---
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,13 @@
+import { Specification } from "../models/Specification";
+
+interface ICreateSpecificationDTO {
+    name: string;
+    description: string;
+}
+
+interface ISpecificationRepository {
+    create({ name, description }: ICreateSpecificationDTO);
+    findByName(name: string): Specification;
+}
+
+export { ISpecificationRepository, ICreateSpecificationDTO };
---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,27 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpeficicationService {
+    constructor(private specificationRepository: ISpecificationRepository) {}
+
+    execute({ name, description }: IRequest) {
+        const specificationAlreadyExists = this.specificationRepository.findByName(
+            name
+        );
+
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+
+        this.specificationRepository.create({
+            name,
+            description,
+        });
+    }
+}
+
+export { CreateSpeficicationService };
