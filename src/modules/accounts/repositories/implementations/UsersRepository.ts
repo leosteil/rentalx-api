ff3c7aecec160329af849fd97edde51bc285d3ba
leosteil <lsteil@inf.ufsm.br>
2021-08-24 22:51:26 -0300

    Adding hash in passwords and improving CreateUserUseCase

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -13,20 +13,22 @@ class UsersRepository implements IUsersRepository {
 
     async create({
         name,
-        username,
         email,
         driver_license,
         password,
     }: ICreateUserDTO): Promise<void> {
         const user = this.repository.create({
             name,
-            username,
             email,
             driver_license,
             password,
         });
 
         await this.repository.save(user);
+    }
+
+    async findByEmai(email: string): Promise<User> {
+        return this.repository.findOne({ email });
     }
 }
 
