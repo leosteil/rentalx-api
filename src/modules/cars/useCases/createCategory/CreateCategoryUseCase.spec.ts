1816d448f7e90192c124a97b63e50bc488d6e896
leosteil <lsteil@inf.ufsm.br>
2021-09-04 17:31:13 -0300

    Adding jest as depency and writing CreateCategory test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategegoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory
+        );
+    });
+
+    it("should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category description Teste",
+        };
+
+        await createCategoryUseCase.execute(category);
+
+        const createdCategory = await categoriesRepositoryInMemory.findByName(
+            category.name
+        );
+
+        expect(createdCategory).toHaveProperty("id");
+    });
+
+    it("should not be able to create a new category with same name", async () => {
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category description Teste",
+            };
+
+            await createCategoryUseCase.execute(category);
+            await createCategoryUseCase.execute(category);
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
